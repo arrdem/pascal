9ae117e695bd0010da6502d8a08c1c5aa60f9a8a
rmckenzie <rmckenzie92@gmail.com>
2013-03-25 17:47:54 -0500

    Indentation tweak

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   ;; :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure   "1.4.0"]
                  [factual/fnparse       "2.3.0"]
                  [lexington             "0.1.1"]]
 
