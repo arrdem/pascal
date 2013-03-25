1b65de210240044aba56738df8fb198caa9ec68f
rmckenzie <rmckenzie92@gmail.com>
2013-03-25 17:47:18 -0500

    Kill now un-needed files

--- src/me/arrdem/pascal/parser/literals.clj
+++
@@ -1,3 +0,0 @@
-(ns me.arrdem.pascal.parser.literals
-  (:require [name.choi.joshua.fnparse     :as fnp]
-            [me.arrdem.pascal.parser.util :as util]))
--- src/me/arrdem/pascal/parser/util.clj
+++
@@ -1,7 +0,0 @@
-(ns me.arrdem.pascal.parser.util
-  (:require [name.choi.joshua.fnparse :as fnp]))
-
-(defmacro deftoken [symbol val]
-  `(def ~symbol
-     (fnp/term
-      #(= (:lexington.tokens/type %1) ~val))))
