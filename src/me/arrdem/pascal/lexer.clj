b4370d9755a310bef7ad9ff67ae2ce52ef3b7b96
rmckenzie <rmckenzie92@gmail.com>
2013-03-24 22:28:22 -0500

    Rewrite using the deftoken and make-lexer for ease of use later

--- src/me/arrdem/pascal/lexer.clj
+++ src/me/arrdem/pascal/lexer.clj
@@ -1,8 +1,9 @@
 (ns me.arrdem.pascal.lexer
     (:require [lexington.lexer       :refer :all]
-              [lexington.utils.lexer :refer :all]))
+              [lexington.utils.lexer :refer :all]
+              [me.arrdem.pascal.util :refer :all]))
 
-(deflexer pascal-base
+(make-lexer pascal-base
 ;;------------------------------------------------------------------------------
 ;; Comments & ignored crap
   :pcomment      #"\(\*.*\*\)"
@@ -11,78 +12,72 @@
 
 ;;------------------------------------------------------------------------------
 ;; Data literals
-  :string        #"(\"[^\"]+\")|('[^']+')"
-  :floatnum      #"(([0-9]+)(e[\+\-]?[0-9]+))|(([0-9]+)\.([0-9]+)(e[\+\-]?[0-9]+)?)"
-  :intnum        #"[0-9]+"
+  (deftoken pstring  string)
+  (deftoken floatnum #"(([0-9]+)(e[\+\-]?[0-9]+))|(([0-9]+)\.([0-9]+)(e[\+\-]?[0-9]+)?)")
+  (deftoken intnum   #"[0-9]+")
 
 ;;------------------------------------------------------------------------------
 ;; Literal tokens
-  :tok/array     "array"
-  :tok/downto    "downto"
-  :tok/function  "function"
-  :tok/of        "of"
-  :tok/repeat    "repeat"
-  :tok/until     "until"
-  :tok/begin     "begin"
-  :tok/else      "else"
-  :tok/goto      "goto"
-  :tok/packed    "packed"
-  :tok/set       "set"
-  :tok/var       "var"
-  :tok/case      "case"
-  :tok/end       "end"
-  :tok/if        "if"
-  :tok/procedure "procedure"
-  :tok/then      "then"
-  :tok/while     "while"
-  :tok/const     "const"
-  :tok/file      "file"
-  :tok/label     "label"
-  :tok/program   "program"
-  :tok/to        "to"
-  :tok/with      "with"
-  :tok/do        "do"
-  :tok/for       "for"
-  :tok/nil       "nil"
-  :tok/record    "record"
-  :tok/type      "type"
+  (deftoken tok_array     "array")
+  (deftoken tok_downto    "downto")
+  (deftoken tok_function  "function")
+  (deftoken tok_of        "of")
+  (deftoken tok_repeat    "repeat")
+  (deftoken tok_until     "until")
+  (deftoken tok_begin     "begin")
+  (deftoken tok_else      "else")
+  (deftoken tok_goto      "goto")
+  (deftoken tok_packed    "packed")
+  (deftoken tok_set       "set")
+  (deftoken tok_var       "var")
+  (deftoken tok_case      "case")
+  (deftoken tok_end       "end")
+  (deftoken tok_if        "if")
+  (deftoken tok_procedure "procedure")
+  (deftoken tok_then      "then")
+  (deftoken tok_while     "while")
+  (deftoken tok_const     "const")
+  (deftoken tok_file      "file")
+  (deftoken tok_label     "label")
+  (deftoken tok_program   "program")
+  (deftoken tok_to        "to")
+  (deftoken tok_with      "with")
+  (deftoken tok_do        "do")
+  (deftoken tok_for       "for")
+  (deftoken tok_nil       "nil")
+  (deftoken tok_record    "record")
+  (deftoken tok_type      "type")
 
 ;;------------------------------------------------------------------------------
 ;; Literal operators
-  :op/add        "+"
-  :op/sub        "-"
-  :op/mul        "*"
-  :op/div        "/"
-  :op/assign     ":="
-  :op/eq         "="
-  :op/ne         "<>"
-  :op/lt         "<"
-  :op/le         "<="
-  :op/ge         ">="
-  :op/gt         ">"
-  :op/point      "^"
-  :op/dot        "."
+  (deftoken op_add        "+")
+  (deftoken op_sub        "-")
+  (deftoken op_mul        "*")
+  (deftoken op_div        "_")
+  (deftoken op_assign     ":=")
+  (deftoken op_eq         "=")
+  (deftoken op_ne         "<>")
+  (deftoken op_lt         "<")
+  (deftoken op_le         "<=")
+  (deftoken op_ge         ">=")
+  (deftoken op_gt         ">")
+  (deftoken op_point      "^")
+  (deftoken op_dot        ".")
 
 ;;------------------------------------------------------------------------------
 ;; Literal deliminators
-  :delim/comma   ","
-  :delim/semi    ";"
-  :delim/colon   ":"
-  :delim/lparen  "("
-  :delim/rparen  ")"
-  :delim/lbrack  "["
-  :delim/rbrack  "]"
-  :delim/dotdot  ".."
+  (deftoken delim_comma   ",")
+  (deftoken delim_semi    ";")
+  (deftoken delim_colon   ":")
+  (deftoken delim_lparen  "(")
+  (deftoken delim_rparen  ")")
+  (deftoken delim_lbrack  "[")
+  (deftoken delim_rbrack  "]")
+  (deftoken delim_dotdot  "..")
 
 ;;------------------------------------------------------------------------------
-  :identifier    #"[a-zA-Z]+"
+  (deftoken identifier    #"[a-zA-Z]+")
   )
-
-(def strfn
-  (fn [v] (apply str (drop 1 (butlast (:lexington.tokens/data v))))))
-
-(def reader
-  (comp read-string #(apply str %1) :lexington.tokens/data))
 
 (def pascal
   (-> pascal-base
@@ -90,8 +85,8 @@
       (discard :pcomment)
       (discard :bcomment)
 
-      (generate-for :identifier :val reader)
-      (generate-for :string     :val strfn)
-      (generate-for :intnum     :val reader)
-      (generate-for :floatnum   :val reader)
+      (generate-for :identifier :val readerfn)
+      (generate-for :pstring    :val strfn)
+      (generate-for :intnum     :val readerfn)
+      (generate-for :floatnum   :val readerfn)
       ))
