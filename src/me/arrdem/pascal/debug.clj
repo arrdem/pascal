c6cdffb77ac0e42ee07ef8bb9788a34aba96cd0d
rmckenzie <rmckenzie92@gmail.com>
2013-03-25 21:34:42 -0500

    Some tweaking to prevent repetition of the prefix line on subsequent debug calls

--- src/me/arrdem/pascal/debug.clj
+++ src/me/arrdem/pascal/debug.clj
@@ -3,17 +3,28 @@
 ;;------------------------------------------------------------------------------
 ;;------------------------------------------------------------------------------
 
-(def ^:dynamic *rule-stack* (atom '("toplevel")))
+(def ^:dynamic rule-stack    (atom '("toplevel")))
 (def ^:dynamic indent-width  (atom 0))
+(def ^:dynamic prn-prefix    (atom true))
 
 (defn stack []
-  (let [s (apply str (interpose "-> " (reverse @*rule-stack*)))]
+  (let [s (apply str (interpose "-> " (reverse @rule-stack)))]
     (reset! indent-width (count s))
     s))
+
+(defn scope-pop []
+  (swap! rule-stack clojure.core/pop)
+  (reset! prn-prefix true))
+
+(defn push [s]
+  (swap! rule-stack conj s)
+  (reset! prn-prefix true))
 
 (defn debug [& msg]
   (let [prefix (stack)
         indent (apply str (repeat @indent-width " "))]
-    (println prefix)
+    (if @prn-prefix
+      (println prefix))
+    (reset! prn-prefix false)
     (doseq [m msg]
       (println indent m))))
