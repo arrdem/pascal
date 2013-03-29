966abdea2f895591a32d3c04947305521317d521
rmckenzie <rmckenzie92@gmail.com>
2013-03-28 22:09:37 -0500

    Implement semantics for variableids, var declarations

--- src/me/arrdem/pascal/semantics.clj
+++ src/me/arrdem/pascal/semantics.clj
@@ -1,57 +1,19 @@
 (ns me.arrdem.pascal.semantics
-  (:require [me.arrdem.pascal.symtab :refer [genlabel! search]]
+  (:require [clojure.pprint :refer [pprint]]
+            [me.arrdem.pascal.symtab :refer [genlabel! install!]]
             [name.choi.joshua.fnparse :as fnp]))
 
-(defn binop [args]
-  (let [[e0 op e1] args]
-    `(~op ~e0 ~e1)))
+(defn variableid-list
+  ([[first [_ rest]]]
+     (cons first rest)))
 
-(defn makeif [[_0 test _1 s elsepart]]
-  (let [else (if elsepart (second elsepart))]
-    `(~'if ~test ~s ~else)))
-
-(defn makewhile [[_0 test _1 s]]
-  (let [label (genlabel!)]
-    `((~'label ~label)
-      (~'if ~test
-          (~'do ~s
-              (~'goto ~label))))))
-
-(defn makerepeat [[_0 s _1 test]]
-  (let [label (genlabel!)]
-    `((~'label ~label)
-      ~s
-      (~'if (~'not ~test)
-            (~'goto ~label)))))
-
-(defn makederef [sym]
-  `(~'deref ~(symbol (:qname (search sym)))))
-
-(defn makeupfor [[_0 id _1 Vi _2 Vf _3 stmnts]]
-  (let [update `(~':= ~(makederef id) (~'+ ~(makederef id) 1))
-        lstart (genlabel!)
-        lend   (genlabel!)]
-    `((~'label ~lstart)
-      (~'if (~'>= ~(makederef id) ~Vf)
-            (~'goto ~lend))
-      (~'do
-        ~'stmnts)
-      ~update
-      (~'goto ~lstart)
-      (~'label ~lend))))
-
-(defn makedownfor [[_0 id _1 Vi _2 Vf _3 stmnts]]
-  (let [update `(~':= ~(makederef id) (~'- ~(makederef id) 1))
-        lstart (genlabel!)
-        lend   (genlabel!)]
-    `((~'label ~lstart)
-      (~'if (~'<= ~(makederef id) ~Vf)
-            (~'goto ~lend))
-      (~'do
-        ~'stmnts)
-      ~update
-      (~'goto ~lstart)
-      (~'label ~lend))))
-
-(defn makefuncall [[sym _ args]]
-  `(funcall ~(symbol sym) ~@args))
+(defn vardecl
+  "Semantics for the vardecl rule in grammar.clj.
+   Enters vars with their types in the symbol table."
+  [[varseq [_ type]]]
+  (doseq [v varseq]
+    (let [v {:name v
+             :type :symbol
+             :type/data type}]
+      (pprint v)
+      (install! v))))
