3702c566a60b667db29e33d1baea1bf5e983e35f
rmckenzie <rmckenzie92@gmail.com>
2013-03-30 00:58:08 -0500

    drop sad from the deps list as it is no longer used

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure   "1.4.0"]
                  [factual/fnparse       "2.3.0"]
-                 [lexington             "0.1.1"]
-                 [me.arrdem.sad         "0.2.0"]]
+                 [lexington             "0.1.1"]]
 
   :main me.arrdem.pascal
   )
