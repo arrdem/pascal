1d4098cf763bc0c7a6c99b10a4666dd383614fe5
rmckenzie <rmckenzie92@gmail.com>
2013-04-02 12:38:03 -0500

    migrated tests to follow the main code

---
+++ test/me/arrdem/compiler/symab_tests.clj
@@ -0,0 +1,7 @@
+(ns me.arrdem.pascal.symtab-tests
+  (:require [clojure.test :refer :all
+            [me.arrdem.pascal.symtab :refer :all])))
+
+(deftest gensym-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
