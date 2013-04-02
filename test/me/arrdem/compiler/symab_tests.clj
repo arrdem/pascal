dbfa5d65e406060736d3e23712de950b75698cfd
rmckenzie <rmckenzie92@gmail.com>
2013-04-02 12:39:24 -0500

    update namespaces

--- test/me/arrdem/compiler/symab_tests.clj
+++ test/me/arrdem/compiler/symab_tests.clj
@@ -1,6 +1,6 @@
-(ns me.arrdem.pascal.symtab-tests
+(ns me.arrdem.compiler.symtab-tests
   (:require [clojure.test :refer :all
-            [me.arrdem.pascal.symtab :refer :all])))
+            [me.arrdem.compiler.symtab :refer :all])))
 
 (deftest gensym-test
   (testing "FIXME, I fail."
