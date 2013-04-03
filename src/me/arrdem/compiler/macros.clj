e4955fc06a93a0dc6ff89d38f17f6e7e1324f4a1
rmckenzie <rmckenzie92@gmail.com>
2013-04-02 23:48:45 -0500

    cleanups, including symbol resolution fixes and the Macro function from pascal.symtab.stdmacros

--- src/me/arrdem/compiler/macros.clj
+++ src/me/arrdem/compiler/macros.clj
@@ -1,4 +1,6 @@
-(ns me.arrdem.compiler.macros)
+(ns me.arrdem.compiler.macros
+  (:require [me.arrdem.compiler.types :refer [typeof]]
+            [me.arrdem.compiler.symtab :refer [search]]))
 
 ;;------------------------------------------------------------------------------
 ;; Predefined macros
@@ -7,13 +9,29 @@
 ;; type data.
 ;;
 ;; MACROS:
-;;   Macros shall have the type :macro, and the key :fn. As with lisp macros,
-;;   macros will be invoked with arguments equal to the tail of the base
+;;   Macros shall have the type :macro, and the key :expander. As with lisp
+;;   macros, macros will be invoked with arguments equal to the tail of the base
 ;;   form and are expected to return either an atom, or a sequence which will be
 ;;   assumed contain macros and will be macroexpanded until such time as the
 ;;   returned value is no longer a list, or is empty, or the expand is equal to
 ;;   the input. Also, macros shall only be defined at the top level and for this
-;;   pascal compiler are not user-definable. Hardcoded only.
+;;   pascal compiler are not user-definable. Hardcoded only. Other language
+;;   implementations may specify macros, but that is beyond the scope of this
+;;   design although trivially implemented.
+
+(defn Macro
+  "Wrapper function equivalent to (deftype Macro) which makes initialization of
+Macro records quick and relatively easy."
+  [name fn]
+  {:name     name
+   :type     :macro
+   :expander fn})
+
+(defn macro?
+  "Predicate to test whether a resolved symbol is a macro or not. Tests the
+:type key, asserting that it is :macro."
+  [sym]
+  (= :macro (typeof sym)))
 
 (defn pmacroexpand
   "An \"outermost first\" macro implementation. Looks up macros from the symbol
@@ -23,7 +41,7 @@ symbol table entries. This exists so that the macro system can be employed first
  at AST generation time to do type conversion and soforth, and later at code
 generation time so that I'm not writing two macro systems when one will do."
   ([expr]
-     (pmacroexpand expr :fn))
+     (pmacroexpand expr :expander))
   ([expr key]
      (if (seq? expr)
        (let [expander (pmacroexpand (first expr) key)
@@ -31,7 +49,7 @@ generation time so that I'm not writing two macro systems when one will do."
                         (name expander)
                         (str expander))
              expander (search expander)
-             expander (if (= :macro (:type expander))
+             expander (if (macro? expander)
                         (get expander key identity))
              res      (if expander
                         (apply expander (rest partial))
@@ -42,9 +60,3 @@ generation time so that I'm not writing two macro systems when one will do."
            (pmacroexpand res key)
            res))
        expr)))
-
-(def ^:dynamic *pmacros*
-  (atom {}))
-
-(defn install!
-  "Install a macro in the symbol table. Note that if the name provided is not a")
