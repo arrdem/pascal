57c465ce4c89f09afd76703ea4a222b2b81e5254
rmckenzie <rmckenzie92@gmail.com>
2013-04-02 20:28:02 -0500

    gratuitous code cleanup here too

--- src/me/arrdem/pascal.clj
+++ src/me/arrdem/pascal.clj
@@ -2,51 +2,52 @@
   (:require [clojure.pprint :refer :all]
             [me.arrdem.pascal.grammar :refer [pascal-program]]
             [me.arrdem.pascal.lexer :refer [pascal]]
-            [me.arrdem.pascal.symtab :refer [*symtab* render-ns]]
+            [me.arrdem.pascal.symtab :refer [pr-symtab]]
             [name.choi.joshua.fnparse :as fnp])
   (:gen-class :main true))
 
-(def code-pp
+(defn pr-code
   "A wrapper around pprint which makes its code-formatting setting more
 easily accessed."
-  #(with-pprint-dispatch code-dispatch (pprint %)))
+  ([c]
+     (with-pprint-dispatch code-dispatch
+       (pprint c))))
 
-(defn build-ast [toks]
-  (fnp/rule-match
-   pascal-program
-   #(println "FAILED: " %)
-   #(println "LEFTOVER: " %2)
-   {:remainder toks}))
+(defn pr-line
+  "Prints an 80 char wide dash line."
+  ([]
+     (println (apply str (repeat 80 \-)))))
+
+(defn build-ast
+  "Wrapper around the grammar and the FNParse rule invocation to make throwing a
+token sequence at the grammar stack easy."
+  ([toks]
+     (fnp/rule-match
+      pascal-program
+      #(println "FAILED: " %)
+      #(println "LEFTOVER: " %2)
+      {:remainder toks})))
 
 (def process-string
   (comp build-ast pascal))
 
-(def fmnt "%-25s : %s")
-
 (defn -main
   "The only valid arguments are targeted files. If there are no targeted files
 then decomp will target stdin as its token source."
-  [& args]
-  (if-not (empty? args)
-    (doseq [f args]
-      (println "attempting to read file" f)
-      (-> f
-          slurp
-          process-string
-          code-pp)
-      (println (apply str (repeat 80 \-)))
-      (doseq [[k v] @*symtab*]
-        (println (format fmnt (str k) (str v))))
-      (reset! *symtab* me.arrdem.pascal.symtab/base_st)
-      nil)
+  ([& args]
+     (if-not (empty? args)
+       (doseq [f args]
+         (println "attempting to read file" f)
+         (-> f
+             slurp
+             process-string
+             code-pp)
+         (pr-symtab)
+         nil)
 
-    (do (-> *in*
-            slurp
-            process-string
-            code-pp)
-        (println (apply str (repeat 80 \-)))
-        (doseq [[k v] @*symtab*]
-          (if (list? k)
-            (println (format fmnt (render-ns k) (str v)))))
-        (reset! *symtab* me.arrdem.pascal.symtab/base_st)
-        nil)))
+       (do (-> *in*
+               slurp
+               process-string
+               code-pp)
+           (pr-symtab)
+           nil))))
