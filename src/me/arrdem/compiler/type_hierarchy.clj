ad1caad65f6076037d12a269cfb182d58fed3367
arrdem <rmckenzie92@gmail.com>
2013-04-10 17:21:48 -0500

    same compiler update for the type hierarchy system

--- src/me/arrdem/compiler/type_hierarchy.clj
+++ src/me/arrdem/compiler/type_hierarchy.clj
@@ -4,49 +4,51 @@
       :author "Reid McKenzie"}
   me.arrdem.compiler.type-hierarchy
   (:require [clojure.set :as set]
-            [loom.graph  :as graph]
-            [loom.alg    :as traversals]))
-
-(def ^:dynamic *type-graph* (atom -type-graph))
+            [me.arrdem.macros :refer [-<n>]]
+            [me.arrdem.compiler :refer [get-typematrix]]
+            [me.arrdem.compiler.symtab :refer [install! search]]
+            [loom.graph :as graph]
+            [loom.alg :as traversals]))
 
 ;;------------------------------------------------------------------------------
 ;; traversal & computation functions
 (defn conversion-path
   "Resolves the type conversion sequence for going between two types. Returns
-a pair of sequences, being the intermediate types (if any) which that argument
-must be cast through to reach the minimum common representation. Eg. a
-resolve-conversion for \"character\" and \"boolean\" should yield
-'((\"integer\") (\"integer\")) as the minimum common type is integer. Note that
-this algorithm assumes that the type tree is a directed graph with no cycles
-where any node M reachable from node N  _fully represents_ the data stored by
-the type of node N."
-  ([type1 type2]
-     (let [t1 (traversals/bf-traverse @*type-graph* type1)
-           t2 (traversals/bf-traverse @*type-graph* type2)
-
-           common (set/intersection (set t1)
-                                    (set t2))
-
-           candidates (map (juxt (partial traversals/shortest-path
-                                          @*type-graph* type1)
-                                 (partial traversals/shortest-path
-                                          @*type-graph* type2))
-                            common)
-           shortest (first (sort (comparator
-                            (fn [p1 p2] (<= (apply + (map count p1))
-                                            (apply + (map count p2)))))
-                       candidates))]
-           shortest)))
-
-;;------------------------------------------------------------------------------
-;; Type matrix manipulation expressions
-(defmacro with-types
-  ([binding & forms]
-     `(binding [*type-graph* ~binding]
-        ~@forms)))
+   a pair of sequences, being the intermediate types (if any) which that
+   argument must be cast through to reach the minimum common representation. Eg.
+   a resolve-conversion for \"character\" and \"boolean\" should yield
+   '((\"integer\") (\"integer\")) as the minimum common type is integer. Note
+   that this algorithm assumes that the type tree is a directed graph with no
+   cycles where any node M reachable from node N  _fully represents_ the data
+   stored by the type of node N."
+  [graph type1 type2]
+  (-<n> [type1 type2]
+        (map (partial traversals/bf-traverse graph) <>)
+        (map set <>)
+        (apply set/intersection <>)
+        (map (apply juxt (map #(partial traversals/shortest-path graph %1)
+                              [type1 type2]))
+             <>)
+        (first (sort (comparator
+                      (fn [& ps]
+                        (apply <=
+                               (map
+                                (comp
+                                 (partial apply +)
+                                 count) ps))))
+                     <>))))
 
 (defn install-transformer!
-  ([from to entry]
-     (swap! *type-graph* graph/add-edges [from to])
-     (install! (assoc entry
-                 :name (transformer-name from to)))))
+  "Installs a type transformation function both in the symbol table and in the
+   compiler's type translation graph. Note that this function depends upon the
+   entry argument featuring argument and return type data. Returns the updated"
+  [compiler entry]
+  (let [path (apply vector
+                    (map (comp :qname search)
+                         ((juxt :type/arg :type/ret) entry)))]
+    (assert (every? (comp not nil?) path))
+    (assert (not (contains? (apply set (graph/edges (get-typematrix compiler)))
+                            path)))
+    (-<n> compiler
+     (graph/add-edges <1> path)
+     (install! <> entry))))
