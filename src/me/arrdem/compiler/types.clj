d85da5a328087b7caa21caf64163013bdbb19f0f
arrdem <rmckenzie92@gmail.com>
2013-04-10 17:30:20 -0500

    nuke the symbol table type API as it was half-assed and the type tree api does it better.

--- src/me/arrdem/compiler/types.clj
+++ src/me/arrdem/compiler/types.clj
@@ -2,7 +2,6 @@
 
 ;;------------------------------------------------------------------------------
 ;; Code for computing field alignments
-
 (defn aligned-offset [t o]
   (let [s       (:size t)
         off-mod (int (/ o s))]
@@ -26,7 +25,6 @@
 
 ;;------------------------------------------------------------------------------
 ;; Type constructors
-
 (defn BasicType [name size]
   (-> {:type :basic :members {}}
       (assoc :name name)
@@ -48,36 +46,8 @@
       (assoc :size size))))
 
 ;;------------------------------------------------------------------------------
-;; Basic types
-
-(def preal    (BasicType "real"    8))
-(def pinteger (BasicType "integer" 4))
-(def pbool    (BasicType "boolean" 4))
-(def pchar    (BasicType "char"    1))
-
-(def ^:dynamic *types*
-  (atom {"real" preal
-         "integer" pinteger
-         "boolean" pbool
-         "char" pchar}))
-
-;;------------------------------------------------------------------------------
 ;; API for interacting with types and type records
-
 (def sizeof   :size)
 (def typeof   :type)
 (def nameof   :name)
 (def children :members)
-
-(defn install!
-  "Installs a type in the types table, tracing the types of the members to
-compute the total size of the record, as well as the indexing offsets to its
-members. Does not check to see if the type name is already taken."
-  ([t]
-     (swap! *types* (:name t) t)))
-
-(defn search
-  "Searches the type table for a given type. Note that type names are
-unqualified and types are considered to be global unlike variables."
-  ([name]
-     (get @*types* name)))
