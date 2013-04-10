4c4249ca10cf79078899d02539128b592162178d
arrdem <rmckenzie92@gmail.com>
2013-04-10 00:07:04 -0500

    update so that it doesn't die trying to rebind *symtab* etc.

--- test/me/arrdem/pascal/core_test.clj
+++ test/me/arrdem/pascal/core_test.clj
@@ -4,25 +4,26 @@
             [me.arrdem.pascal.test-text :as data]
 
             [me.arrdem.pascal :refer [process-string build-ast]]
-            [me.arrdem.pascal.symtab :refer [search with-p-symtab]]))
+            [me.arrdem.pascal.symtab :refer [search with-p-symtab clear!]]))
 
 (defmacro full-test-case [sym val]
   `(deftest ~sym
      (testing
-         (with-p-symtab
-          (let [result# (process-string (:text ~val))]
-          ;; check AST result...
-          (is (= result# (:ast ~val)))
-          ;; check symbol table contents...
-          (doseq [s# (:symbols ~val)]
-            (is (= (search (:name s#)) s#)
-                (str "symbol " (:qname s#) " was not defined!"))))))))
+         (let [result# (process-string (:text ~val))]
+           ;; check AST result...
+           (is (= result# (:ast ~val)))
+           ;; check symbol table contents...
+           (doseq [s# (:symbols ~val)]
+             (is (= (search (:name s#)) s#)
+                 (str "symbol " (:qname s#) " was not defined!")))))
+     (clear!)))
 
 ;;------------------------------------------------------------------------------
 ;; the big test cases over assignment inputs...
 
 (full-test-case triv-pas-test data/triv-pas)    ;; parser assignment 1
-(full-test-case trivb-pas-test data/trivb-pas)  ;; half of parser assignment 2
+(full-test-case trivb-pas-test data/trivb-pas)  ;; parser assignment 1
+(full-test-case trivb-pas-test data/graph1-pas) ;; parser assignment 2
 
 ;;------------------------------------------------------------------------------
 ;; TODO: partial test cases over subsets of the grammar
