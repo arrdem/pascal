501a0dc17b8a4d976c2267b8a25744954c91b8e8
arrdem <rmckenzie92@gmail.com>
2013-04-15 11:13:36 -0500

    rework to operate on an arbitrary standalone graph

--- src/me/arrdem/compiler/type_hierarchy.clj
+++ src/me/arrdem/compiler/type_hierarchy.clj
@@ -7,22 +7,20 @@
             [loom.graph  :as graph]
             [loom.alg    :as traversals]))
 
-(def ^:dynamic *type-graph* (atom -type-graph))
-
 ;;------------------------------------------------------------------------------
 ;; traversal & computation functions
 (defn conversion-path
   "Resolves the type conversion sequence for going between two types. Returns
-a pair of sequences, being the intermediate types (if any) which that argument
-must be cast through to reach the minimum common representation. Eg. a
-resolve-conversion for \"character\" and \"boolean\" should yield
-'((\"integer\") (\"integer\")) as the minimum common type is integer. Note that
-this algorithm assumes that the type tree is a directed graph with no cycles
-where any node M reachable from node N  _fully represents_ the data stored by
-the type of node N."
-  ([type1 type2]
-     (let [t1 (traversals/bf-traverse @*type-graph* type1)
-           t2 (traversals/bf-traverse @*type-graph* type2)
+   a pair of sequences, being the intermediate types (if any) which that
+   argument must be cast through to reach the minimum common representation. Eg.
+   a resolve-conversion for \"character\" and \"boolean\" should yield
+   '((\"integer\") (\"integer\")) as the minimum common type is integer. Note
+   that this algorithm assumes that the type tree is a directed graph with no
+   cycles where any node M reachable from node N  _fully represents_ the data
+   stored by the type of node N."
+  ([graph type1 type2]
+     (let [t1 (traversals/bf-traverse graph type1)
+           t2 (traversals/bf-traverse graph type2)
 
            common (set/intersection (set t1)
                                     (set t2))
@@ -37,16 +35,3 @@ the type of node N."
                                             (apply + (map count p2)))))
                        candidates))]
            shortest)))
-
-;;------------------------------------------------------------------------------
-;; Type matrix manipulation expressions
-(defmacro with-types
-  ([binding & forms]
-     `(binding [*type-graph* ~binding]
-        ~@forms)))
-
-(defn install-transformer!
-  ([from to entry]
-     (swap! *type-graph* graph/add-edges [from to])
-     (install! (assoc entry
-                 :name (transformer-name from to)))))
