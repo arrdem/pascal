9bd3bf5532ea32113629d6f355a75166382558c7
arrdem <rmckenzie92@gmail.com>
2013-04-15 11:22:44 -0500

    rework to fix namespaces, deps and ditch the atom nonsense

--- test/me/arrdem/compiler/type_hierarchy_test.clj
+++ test/me/arrdem/compiler/type_hierarchy_test.clj
@@ -1,16 +1,16 @@
 (ns me.arrdem.compiler.type-hierarchy-test
   (:require [clojure.test :refer :all]
-            [me.arrdem.pascal.types :refer :all]))
+            [me.arrdem.compiler.type-hierarchy :refer :all]
+            [me.arrdem.pascal.types :refer [-type-graph]]
+            [loom.graph :as graph]))
 
 (deftest self-conversion
-  (with-types (atom -type-graph)
-    (doseq [t ["integer" "real" "character" "boolean"]]
-      (is (= (conversion-path t t) [nil nil])))))
+  (doseq [t ["integer" "real" "character" "boolean"]]
+    (is (= (conversion-path -type-graph t t) [nil nil]))))
 
 (deftest int-conversion
-  (with-types (atom -type-graph)
-    (is (= (conversion-path "character" "integer")
-           ['("character" "integer") nil]))
+  (is (= (conversion-path -type-graph "character" "integer")
+         ['("character" "integer") nil]))
 
-    (is (= (conversion-path "boolean" "integer")
-           ['("boolean" "integer") nil]))))
+  (is (= (conversion-path -type-graph "boolean" "integer")
+         ['("boolean" "integer") nil])))
