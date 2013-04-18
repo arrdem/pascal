d2cc04fb79d43d51603de9c9dbb200a64a2d84ae
arrdem <rmckenzie92@gmail.com>
2013-04-18 01:52:25 -0500

    done in by the type of (rest) being lazyseq..

--- src/me/arrdem/compiler/macros.clj
+++ src/me/arrdem/compiler/macros.clj
@@ -14,26 +14,33 @@
     (= "macro" (typeof obj))
     (catch Exception e false)))
 
-(defn pmacroexpand
+(declare pmacroexpand)
+
+(defn _pmacroexpand
   "An \"outermost first\" macro implementation. Looks up macros from the symbol
    table, and applies them if possible."
   [expr]
-  (if (list? expr)
-    (let [expander (pmacroexpand (first expr))
-          expandfn (cond
-                    (macro? expander) expander
-                    (string? expander) (search expander)
-                    (symbol? expander) (search (name expander))
-                    true nil)
-          expandfn (when (macro? expandfn)
-                     (.expander expandfn))
-          res (if (fn? expandfn)
-                (expandfn (rest expr))
-                expr)
-          res (cons (first res)
-                    (doall (map pmacroexpand (rest res))))]
-      (if (and (fn? expandfn) ;; possibility of recursive macro
-               (not (= res expr))) ;; prevent non-transforming recursion
-        (pmacroexpand res)
-        res))
+  (println "; macroexpand got param of type " (type expr))
+  (if (seq? expr)
+    (do (println "; macroexpanding " expr)
+        (let [expander (pmacroexpand (first expr))
+              expandfn (cond
+                        (macro? expander) expander
+                        (string? expander) (search expander)
+                        (symbol? expander) (search (name expander))
+                        true nil)
+              expandfn (when (macro? expandfn)
+                         (.expander expandfn))
+              res (if (fn? expandfn)
+                    (expandfn (apply list (rest expr)))
+                    expr)]
+          (println "; macroexpanding intermediate state " res)
+          (let [res (cons (first res)
+                          (doall (map pmacroexpand (apply list (rest res)))))]
+            (if (and (fn? expandfn) ;; possibility of recursive macro
+                     (not (= res expr))) ;; prevent non-transforming recursion
+              (pmacroexpand res)
+              res))))
     expr))
+
+(def pmacroexpand (memoize _pmacroexpand))
