da1573f71369d9f1519fe0cb2f1084bfec2ea051
arrdem <rmckenzie92@gmail.com>
2013-04-18 12:57:25 -0500

    patch conversion-path to behave for several argument types

--- src/me/arrdem/compiler/type_hierarchy.clj
+++ src/me/arrdem/compiler/type_hierarchy.clj
@@ -17,20 +17,20 @@
    that this algorithm assumes that the type tree is a directed graph with no
    cycles where any node M reachable from node N  _fully represents_ the data
    stored by the type of node N."
-  ([graph type1 type2]
-     (let [t1 (traversals/bf-traverse graph type1)
-           t2 (traversals/bf-traverse graph type2)
+  ([graph & types]
+     (let [traversals (map (partial traversals/bf-traverse graph)
+                           types)
+           common (apply set/intersection
+                         (map set traversals))
 
-           common (set/intersection (set t1)
-                                    (set t2))
-
-           candidates (map (juxt (partial traversals/shortest-path
-                                          graph type1)
-                                 (partial traversals/shortest-path
-                                          graph type2))
+           candidates (map (apply juxt
+                                  (map #(partial traversals/shortest-path graph %1)
+                                       types))
                             common)
-           shortest (first (sort (comparator
-                            (fn [p1 p2] (<= (apply + (map count p1))
-                                            (apply + (map count p2)))))
-                       candidates))]
+           shortest (first
+                     (sort
+                      (comparator
+                       (fn [p1 p2] (<= (apply + (map count p1))
+                                       (apply + (map count p2)))))
+                      candidates))]
            shortest)))
