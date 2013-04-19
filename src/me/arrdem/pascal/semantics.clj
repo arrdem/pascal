a67bcb21306a43fa900ef88c566f68770a6f0aee
arrdem <rmckenzie92@gmail.com>
2013-04-18 19:46:01 -0500

    huuuuuge restructure of semantics
    - massively improved variable and array structuring
    - OCD comments
    - section delimiters
    and much more inside this patch!

--- src/me/arrdem/pascal/semantics.clj
+++ src/me/arrdem/pascal/semantics.clj
@@ -5,44 +5,30 @@
                                                 ->ArrayType sizeof fields
                                                 ->EnumType ->RecordEntry
                                                 ->PointerType ->ThinType
-                                                valueof]]
+                                                valueof follow field-offset]]
             [me.arrdem.compiler.types :refer [->RecordType]]
             [me.arrdem.compiler.symtab :refer [genlabel! install!
                                                search gensym! render-ns]]
             [me.arrdem.compiler.macros :refer [pmacroexpand macro?]]
             [me.arrdem.pascal.ast :refer :all]
-
             [name.choi.joshua.fnparse :as fnp]))
 
-;;------------------------------------------------------------------------------
-;; TODO: reduce this out, need to eliminate calls to the other three from the
-;;       grammar before I can delete them here.
-
 (defn tail-cons
+  "Basic cons operation for joining recursively defined eliminated lists.
+   Eg. a, b, c, d, e -> (a (b (c (d (e nil))))) which this takes down to
+   (a b c d e) for ease of use."
   [[s [_ rest]]]
   (cons s rest))
 
-(def cons-ht tail-cons)
-(def variableid-list tail-cons)
-(def vardecls tail-cons)
-
 ;;------------------------------------------------------------------------------
 ;; NEW generation code..
-
-(defn vardecl
-  "Semantics for the vardecl rule in grammar.clj.
-   Enters vars with their types in the symbol table."
-  [[varseq _ type]]
-  (doseq [v varseq]
-    (let [v (->VariableType v (search (nameof type)) nil)]
-      (dbg-install v)))
-  (map abs-name varseq))
-
-(defn variable-declaration
-  [[_ decls]]
-  (apply makecomment "defined variables" (reduce concat decls)))
+;; TODO: organize this in some vaguely sane way so that it relates to the code
+;;       in grammar.clj more nicely. Maybe even modularize both the core grammar
+;;       and the semantics then inline the sub-groups?
 
 (defn const-assign
+  "Creates & installs a typed (generated) variable to represent constants,
+   returning a variable identifier which the constant's symbol will shadow."
   [[id _ v]]
   (let [v (->VariableType id (search (typeof v)) v)]
     (dbg-install v)))
@@ -53,23 +39,17 @@
     (apply makecomment "got constant decl group:" (cons c0 cs))))
 
 (defn string
+  "Generates an anonymous string variable & returns its identifier so that other
+   code can use (and shadow) the constant string val it represents."
   [s]
   (let [sym (->VariableType (gensym! "str_") "string" s)]
     (dbg-install sym)))
 
-(defn snum
-  [prefix [sign? rval]]
-  (let [factor (case sign?
-                 (+ nil) 1
-                 (-) -1)
-        sym (gensym! (str prefix "_"))
-        val (->VariableType sym (typeof rval) (* factor rval))]
-    (dbg-install val)))
-
-(def integer (partial snum "integer"))
-(def real (partial snum "real"))
-
 (defn label-declaration
+  "Generates variables with the _string_ values of labels, integer type and a
+   constant value equal to the genlabel! when they were parsed. Installs these
+   ``variables'' in the symbol table so that they can be looked up by a special
+   case in the goto production code."
   [[_l l0 ls]]
   (let [ls (map second ls)
         labels (map str (cons l0 ls))]
@@ -77,136 +57,19 @@
       (install! (->VariableType l (search "integer") (genlabel!))))
     (apply makecomment "found label declarations" labels)))
 
-(defn variable
-  [[id postfixes]]
-  (let [id (abs-name id)]
-    (assert id)
-    (if-not (empty? postfixes)
-      (apply (partial e-> id) postfixes)
-      id)))
-
-(defn additive-expression
-  [[me tail]]
-  (if-let [[op adxpr] tail]
-    (binop me op adxpr)
-    me))
-
-(defn assignment
-  [[target assignop expr]]
-  (binop target ':= expr))
-
-(defn pascal-program
-  [[[_0 id] heading _1 block _2]]
-  `(~'program ~id ~@heading ~@block))
-
-(defn program-heading
-  [[_l ids _r]]
-  (map makeprogn-v
-       (map list ids)))
-
-(defn block
-  [[comments progn]]
-  (concat comments (list progn)))
-
-(defn block2progn
-  [[_0 exprs _1]]
-  (println "; in block2progn..")
-  (->> exprs
-      (remove nil?)
-      (makeprogn-v)
-      (pmacroexpand)))
-
-(defn for-downto
-  [[s0 _ sf]]
-  [s0 `(~'- 1) '>= sf])
-
-(defn for-to
-  [[s0 _ sf]]
-  [s0 `(~'+ 1) '<= sf])
-
-(defn for-stmnt [[_0 id _1 flist _3 stmnt]]
-  (let [[Vi update comp end] flist
-        lstart (genlabel!)
-        id     (abs-name (search id))]
-    (makeprogn
-      [(makelabel lstart)
-       (binop id ':= Vi)
-       (makeif `(~comp ~id ~end)
-               (makeprogn [stmnt
-                           (binop id ':= (concat update (list id)))
-                           (makegoto lstart)]))
-       ])))
-
-(defn repeat-stmnt
-  [[_rep stmnts _unt expr]]
-  (let [lbl (genlabel!)]
-    (makeprogn
-     [(makelabel lbl)
-      (makeprogn stmnts)
-      (makeif `(~'not ~expr)
-              (makegoto lbl))
-      ])))
-
-(defn procinvoke
-  [[id [_0 params _1]]]
-  (let [f (search id)]
-    (if (macro? f)
-      (concat (list (nameof f))
-              params)
-      (makefuncall id params))))
-
-(defn identifier
-  ([id] (abs-name id)))
-
-(defn unary-expression
-  [[op expr]]
-  (let [form (case op
-               (+) nil
-               (-) `(~'* -1)
-               (not) `(~'not)
-               (nil) nil)]
-    (if form
-      (concat form (list expr))
-      expr)))
-
-(defn statements
-  [[_0 stmnts _1]]
-  (makeprogn stmnts))
-
 (defn point-type
+  "Stupid little routine to compute the name of a pointer type based on the
+   name of the type to which it points."
   [[_point type]]
   (str "^" type))
 
-(defn var-index
-  [[_lb subscripts _rb]]
-  (map partial-make-aref
-       (if (seq? subscripts)
-         subscripts [subscripts])))
-
-(defn install-arrtype
-  [[_arr _0 index-list _1 _of type]]
-  (loop [t (reverse index-list)
-         child-type (search type)]
-    (let [my-ind (first t)
-          my-ind (if (instance? me.arrdem.compiler.symbols.RecordType  my-ind)
-                   my-ind (typeof (search my-ind)))
-          my-len  (count (fields my-ind))
-          my-name (str (nameof child-type) "-" my-len)
-          self (->ArrayType
-                 (str (nameof child-type) "-" my-len)
-                 (* (sizeof child-type) my-len)
-                 (zipmap (keys (fields my-ind))
-                         (repeat my-len child-type)))]
-      (install! self)
-      (if-not (empty? (rest t))
-        (recur (rest t)
-               self)
-        self))))
+;;------------------------------------------------------------------------------
+;; Compound types...
 
 (defn install-range
   [[low _r high]]
   (let [c (- (inc high) low)
-        i (search "integer")
+        i "integer"
         t (->RecordType (gensym! (str "range-" low "->" high "_"))
                         (map (fn [x y] {:value x :name x :type y})
                              (range low (inc high))
@@ -229,11 +92,44 @@
 
 (defn apply-type
   [syms type]
-  (let [t (typeof
-           (if (string? type)
-             (search type) type))]
+  ;; (println "[apply-type] syms: " syms)
+  ;; (println "[apply-type] type: " type)
+  (let [t (typeof (search type))]
      (map #(->RecordEntry %1 t nil)
           syms)))
+
+(defn build-array [index-range-list basictype]
+  (println "[build-array] " index-range-list)
+  (loop [index-range-list (->> index-range-list
+                               reverse
+                               (map search)
+                               (map (fn [x] (range (count (fields x))))))
+         state (list (search basictype))]
+    (println "[build-array] - " index-range-list)
+    (println "[build-array] - " (count state))
+    (let [index-list (first index-range-list)
+          index-range-list (rest index-range-list)
+          type (first state)]
+      (println "[build-array] - type - " (nameof type))
+      (let [substruct (->RecordType (gensym! "__array_")
+                                    (apply-type index-list type))]
+        (install! substruct)
+        (if-not (empty? index-range-list)
+          (recur index-range-list
+                 (cons substruct state))
+          (cons substruct state))))))
+
+(defn install-arrtype
+  "Computes and installs an array type, being a record with multi-integer
+   addressed fields."
+  [[_arr _0 index-list _1 _of type]]
+  (println "[install-arrtype] index-list: " index-list)
+  (println "[install-arrtype] type: " type)
+  (let [subarray-seq (build-array index-list type)]
+    (println "[install-arrtype] build-array returned OK")
+    (install! (->ArrayType (gensym! "array_")
+                           (sizeof (first subarray-seq))
+                           subarray-seq))))
 
 (defn record-field
   [[idlist _ type]]
@@ -260,6 +156,174 @@
   [[_t decls _e]]
   (apply makecomment "got type definitions:" (map #(. %1 nameof) decls)))
 
+;;------------------------------------------------------------------------------
+;; Variables....
+(defn vardecl
+  "Semantics for the vardecl rule in grammar.clj.
+   Enters vars with their types in the symbol table."
+  [[varseq _ type]]
+  (doseq [v varseq]
+    (let [v (->VariableType v (search (nameof type)) nil)]
+      (dbg-install v)))
+  (map abs-name varseq))
+
+(defn variable-declaration
+  "Invoked to generate the comment group for variable declaration parts"
+  [[_ decls]]
+  (apply makecomment "defined variables" (reduce concat decls)))
+
+(defn var-dot [[_dot id]]
+  (fn [x]
+    (list (list '. id)
+          (get (fields x) id))))
+
+(defn var-point [_]
+  (fn [obj]
+    (println "; [var-point] " (nameof obj) " is " (nameof (follow obj) ))
+    (list (list (symbol "^"))
+          (follow obj))))
+
+(defn var-index
+  [[_lb subscripts _rb]]
+  (fn [obj]
+    (list (partial-make-aref
+           (field-offset
+            obj (seq subscripts)))
+          (typeof (last (fields obj))))))
+
+(defn variable
+  "Generates the appropriate aref & pointer expressions for indexing into a
+   variable of some type. Postfixes is assumed to be a sequence of
+   single-argument functions which expect the symbol value of the variable as
+   their only argument and which expand into index operations such as aref and
+   the deference operator."
+  [[id postfixes]]
+  (let [self (search id)
+        res (reduce (fn [state f]
+                      (println "; [variable-fn] f:" f)
+                      (let [[expr new-state] (f (:sym state))]
+                        (-> state
+                            (assoc :sym new-state)
+                            (update-in [:ops] conj expr))))
+                    {:sym self} postfixes)]
+    (assert search)
+    (println "; [variable] " id)
+    (println "; [variable] " (:ops res))
+    (if-not (empty? postfixes)
+      (apply e-> (nameof self) (reverse (:ops res)))
+      id)))
+
+;;------------------------------------------------------------------------------
+;; Arithmetic expressions...
+(defn additive-expression
+  [[me tail]]
+  (if-let [[op adxpr] tail]
+    (binop me op adxpr)
+    me))
+
+(defn assignment
+  [[target assignop expr]]
+  (binop target ':= expr))
+
+(defn unary-expression
+  [[op expr]]
+  (let [form (case op
+               (+) nil
+               (-) `(~'* -1)
+               (not) `(~'not)
+               (nil) nil)]
+    (if form
+      (concat form (list expr))
+      expr)))
+
+;;------------------------------------------------------------------------------
+;; Basic program control structure transforms
+(defn pascal-program
+  [[[_0 id] heading _1 block _2]]
+  `(~'program ~id ~@heading ~@block))
+
+(defn program-heading
+  [[_l ids _r]]
+  (map makeprogn-v
+       (map list ids)))
+
+(defn block
+  [[comments progn]]
+  (concat comments (list progn)))
+
+(defn block2progn
+  [[_0 exprs _1]]
+  (println "; in block2progn..")
+  (->> exprs
+      (remove nil?)
+      (makeprogn-v)
+      (pmacroexpand)))
+
+;;;;----------------------------------------------------------------------------
+;;;; the FOR control structure
+(defn for-downto
+  [[s0 _ sf]]
+  [s0 `(~'- 1) '>= sf])
+
+(defn for-to
+  [[s0 _ sf]]
+  [s0 `(~'+ 1) '<= sf])
+
+(defn for-stmnt [[_0 id _1 flist _3 stmnt]]
+  (let [[Vi update comp end] flist
+        lstart (genlabel!)
+        id     (abs-name (search id))]
+    (makeprogn
+      [(makelabel lstart)
+       (binop id ':= Vi)
+       (makeif `(~comp ~id ~end)
+               (makeprogn [stmnt
+                           (binop id ':= (concat update (list id)))
+                           (makegoto lstart)]))
+       ])))
+
+;;;;----------------------------------------------------------------------------
+;;;; the REPEAT control structure
+(defn repeat-stmnt
+  [[_rep stmnts _unt expr]]
+  (let [lbl (genlabel!)]
+    (makeprogn
+     [(makelabel lbl)
+      (makeprogn stmnts)
+      (makeif `(~'not ~expr)
+              (makegoto lbl))
+      ])))
+
+;;;;----------------------------------------------------------------------------
+;;;; WHILE structure
+(defn while-stmnt
+  [[_while e _do stmnt]]
+  (makewhile e stmnt))
+
+;;;;----------------------------------------------------------------------------
+;;;; IF structures
+(defn ift
+  [[_if expr _then stmnt]]
+  (makeif expr stmnt))
+
+(defn ifte
+  [[_if expr _then stmnt _else stmnt2]]
+  (makeif expr stmnt stmnt2))
+
+;;;;----------------------------------------------------------------------------
+;;;; Other statement parts...
+(defn procinvoke
+  [[id [_0 params _1]]]
+  (let [f (search id)]
+    (if (macro? f)
+      (concat (list (nameof f))
+              params)
+      (makefuncall id params))))
+
+(defn statements
+  [[_0 stmnts _1]]
+  (makeprogn stmnts))
+
 (defn label-stmnt
   [[lbl _colon stmnt]]
   (makeprogn
@@ -278,15 +342,3 @@
       search
       valueof
       makegoto))
-
-(defn ift
-  [[_if expr _then stmnt]]
-  (makeif expr stmnt))
-
-(defn ifte
-  [[_if expr _then stmnt _else stmnt2]]
-  (makeif expr stmnt stmnt2))
-
-(defn while-stmnt
-  [[_while e _do stmnt]]
-  (makewhile e stmnt))
