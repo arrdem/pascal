15641b6b9d3fe53d5147873b2b958b6cc77af331
arrdem <rmckenzie92@gmail.com>
2013-04-21 16:32:55 -0500

    this is longer but should work

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject me.arrdem/pascal (slurp "VERSION")
+(defproject me.arrdem/pascal (apply str (replace {\newline ""}
+                                                 (slurp "VERSION")))
   :description "A Pascal lexer, parser and AST generator"
   :url "http://github.com/arrdem/pascal"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure   "1.4.0"]
-                 [factual/fnparse       "2.3.0"]
-                 [lexington             "0.1.1"]
-                 [jkkramer/loom         "0.2.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [factual/fnparse "2.3.0"]
+                 [jkkramer/loom "0.2.0"]]
+                 [lexington "0.1.1"]
   :main me.arrdem.pascal)
