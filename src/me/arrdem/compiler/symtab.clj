1e95fc5ff8a329391a8cf50795272e3d21a5ce9d
arrdem <rmckenzie92@gmail.com>
2013-04-22 14:05:25 -0500

    tested and working side-effect isolation for the symbol table

--- src/me/arrdem/compiler/symtab.clj
+++ src/me/arrdem/compiler/symtab.clj
@@ -1,6 +1,5 @@
 (ns me.arrdem.compiler.symtab
-  (:require [clojure.string :refer [split]]
-            [me.arrdem.compiler :refer [nameof]]
+  (:require [me.arrdem.compiler :refer [nameof]]
             [me.arrdem.compiler.namespace :refer :all]))
 
 (defn- ninc
@@ -14,7 +13,6 @@
 ;; The symbol table
 
 (def ^:dynamic *symtab*
-  "Used to track all symbols."
   (atom {}))
 
 (defn gensym
@@ -24,7 +22,7 @@
    numeric part of the name. Returns a string being the prefix argument or
    \"G__\" followed by the string render of the gensym counter before it was
    incremented."
-  ([table] (gensym! table "G__"))
+  ([table] (gensym table "G__"))
   ([table s] (-> table
                  (update-in table [:gensym] ninc)
                  :gensym
@@ -50,50 +48,64 @@
   [table]
   (assoc table :label 0))
 
-;;------------------------------------------------------------------------------
-;; Manipulation routines for the various symbol registries which this system
-;; uses. Other namespaces define partials atop these meta-functions which are
-;; then used to manipulate the macro system, the type system and soforth.
-
 (defn symbol-install
   "Meta symbol installer. Takes a namespace structure and a record as arguments,
    and performs the appropriate swap! respecting the namespacing stack."
   [table ns-stack sym]
   (let [path (concat ns-stack (list (nameof sym)))
         sym (assoc sym :qname (render-ns path))]
-    (assoc-in table path sym)
-    sym))
+    (assoc-in table path sym)))
 
-(defn search
+(defn __search-symtab
   "Recursively searches the symbol table for a symbol with an argument name.
    Returns the symbol map if such a symbol exists. Failure behavior is
    undefined, but the returning a nil value and throwing an exception are both
    acceptable."
   [table ns-stack sym]
   (let [qualified-sym (concat ns-stack (list sym))
-        rstack (rest stack)]
-    (or (if-let [v (get-in table qualified-sym)]
-          v (when (empty? stack) nil))
-        (when-not (empty? stack)
+        rstack (rest ns-stack)]
+    (println "; [search-symtab]" table)
+    (println "; [search-symtab] -" qualified-sym)
+
+    (or (get-in table qualified-sym)
+        (if (empty? ns-stack)
+          nil
           (recur table sym rstack)))))
 
+(defn search-symtab
+  "A wrapper around stack-search wich provides the base case logic required to
+parse fully qualified names into a full stack path. Defaults to using
+stack-search before returning a failure result."
+  [map name]
+  (let [stack (decomp-ns name)
+        name (last stack)
+        stack (butlast stack)
+        stack (if (empty? stack) (get-ns) stack)]
+    (__search-symtab map stack name)))
+
+;;------------------------------------------------------------------------------
 ;;------------------------------------------------------------------------------
 ;; side-effectful wrappers around the above stateless API
 (defmacro with-symtab
-  ""
   [table & body]
-  `(binding [*symtab* table]
+  `(binding [*symtab* (atom ~table)]
     ~@body))
 
 (defn set-symtab!
-  ""
   [table]
   (alter-var-root #'*symtab*
-                  (constantly table)))
+                  (constantly (atom table))))
 
 (defn symtab!
-  ""
   [& fns]
-  (set-symtab! {})
-  (doseq [f fns]
-    (f)))
+  (with-symtab
+    (set-symtab! {})
+    (doseq [f fns]
+      (f))))
+
+(defn search [symbol]
+  (search-symtab @*symtab* symbol))
+
+(defn install! [symbol]
+  (swap! *symtab* symbol-install (get-ns) symbol)
+  (search (nameof symbol)))
