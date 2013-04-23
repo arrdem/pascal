1e2f377b9d8816f69e6bed5d6c296224fa80dfa1
arrdem <rmckenzie92@gmail.com>
2013-04-23 17:25:40 -0500

    check argument types fr grins

--- src/me/arrdem/pascal/semantics.clj
+++ src/me/arrdem/pascal/semantics.clj
@@ -1,7 +1,7 @@
 (ns me.arrdem.pascal.semantics
   (:require [me.arrdem.compiler :refer [nameof typeof sizeof fields
                                         valueof follow field-offset addrof
-                                        return-type]]
+                                        return-type valid-invokation?]]
             [me.arrdem.compiler.types :refer [->RecordType]]
             [me.arrdem.compiler.macros :refer [pmacroexpand macro?]]
             [me.arrdem.compiler.symtab :refer [genlabel! install!
@@ -358,9 +358,13 @@
   (if (macro? id)
     (concat (list (nameof id))
             params)
-    (with-meta
-      (makefuncall id params)
-      {:type (return-type (search id))})))
+    (do
+      (assert (valid-invokation? (search id)
+                                 (apply list
+                                        (map typeof params))))
+      (with-meta
+        (makefuncall id params)
+        {:type (return-type (search id))}))))
 
 (defn statements
   [[_0 stmnts _1]]
