70b3cd1093682bbc57880972947570dfb42cbf96
arrdem <rmckenzie92@gmail.com>
2013-05-07 23:04:04 -0500

    clean up a lot of float register allocation crap which I can't use anyway

--- src/me/arrdem/compiler/code_generation.clj
+++ src/me/arrdem/compiler/code_generation.clj
@@ -10,10 +10,12 @@
 ;; The register state is defined to be a map, of the structure
 ;;
 ;; {
-;;  :free-regs #{} (type clojure.lang.set)
-;;  :used-regs #{} (type clojure.lang.set)
-;;  :free-fregs #{}
-;;  :used-fregs #{}
+;;  :free-regs #{}   (type clojure.lang.set)
+;;  :used-regs #{}   (type clojure.lang.set)
+;;  :preamble  {     (type clojure.lang.map)
+;;    :code  ()      (type clojure.lang.list)
+;;    :installed #{} (type clojure.lang.set)
+;;  }
 ;; }
 ;;
 ;; The idea is that the union of freed-regs and used-regs is constant, being the
@@ -25,83 +27,60 @@
 (def free-regs :free-regs)
 (def used-regs :used-regs)
 
-(def free-float-regs :free-fregs)
-(def used-float-regs :used-fregs)
-
 (defn register?
   "Predicate used to test whether, in the context of the argument state, the
    argument register is in fact a valid register."
   [state reg]
-  (contains? (reduce union
-                     (vals state))
+  (contains? (union (free-regs state)
+                    (used-regs state))
              reg))
 
 ;;------------------------------------------------------------------------------
 ;; (register) state manipulating functions
 
 (defn- -use-reg
-  ([state reg] (-use-reg state reg free-regs used-regs))
-  ([state reg free-key used-key]
-     (-> state
-         (update-in [free-key] difference (set [reg]))
-         (update-in [used-key] union (set [reg])))))
+  [state reg]
+  (-> state
+      (update-in [free-regs] difference (set [reg]))
+      (update-in [used-regs] union (set [reg]))))
 
 (defn use-reg
   "Allocates a register, returning a new state record in which the argument
    register is marked as used. May or may not warn if the argument register is
    already marked as used, returns the same state in this case."
-  ([state register] (use-reg state register free-regs used-regs))
-  ([state register free-key used-key]
-     (let [free (free-key state)
-           used (used-key state)]
-       (cond (not (register? state register))
-             state
+  [state register]
+  (let [free (free-regs state)
+        used (used-regs state)]
+    (cond (not (register? state register))
+          state
 
-             (contains? free register)
-             (-use-reg state register free-key used-key)
+          (contains? free register)
+          (-use-reg state register)
 
-             (contains? used register)
-             ;; optional warning case
-             (-use-reg state register free-key used-key)))))
+          (contains? used register)
+          ;; optional warning case
+          (-use-reg state register))))
 
 (defn free-reg
   "Deallocates a register, returning a new state record in which the argument
    register is marked as free. May warn if the argument register is already
    marked used."
-  ([state register] (free-reg state register :integer))
-  ([state register type]
-     (case type
-       (:integer)
-           (if (register? state register)
-             (-> state
-                 (update-in [free-regs] union (set [register]))
-                 (update-in [used-regs] difference (set [register])))
-             state)
-       (:float :real)
-           (if (register? state register)
-             (-> state
-                 (update-in [free-float-regs] union (set [register]))
-                 (update-in [used-float-regs] difference (set [register])))
-             state))))
+  [state register]
+  (if (register? state register)
+    (-> state
+        (update-in [free-regs] union (set [register]))
+        (update-in [used-regs] difference (set [register])))
+    state))
 
 (defn reg-alloc
   "Allocates a register, returning a pair (new-state, register) if there is at
    least one unused register in the argument state. If there are no unused
    registers in the state then a pair (state, nil) is returned."
-  ([state] (reg-alloc state :integer))
-  ([state type]
-     (case type
-       (:integer)
-           (let [free (free-regs state)]
-             (if (empty? free)
-               [state nil]
-               [(use-reg state (first free)) (first free)]))
-       (:float :real)
-           (let [free (free-float-regs state)]
-             (if (empty? free)
-               [state nil]
-               [(use-reg state (first free) free-float-regs used-float-regs)
-                (first free)])))))
+  [state]
+  (let [free (free-regs state)]
+    (if (empty? free)
+      [state nil]
+      [(use-reg state (first free)) (first free)])))
 
 ;;------------------------------------------------------------------------------
 ;; genarith and supporting functions
@@ -306,7 +285,7 @@
          "st(0)"]
 
     ("integer" nil)
-        (let [[state dst] (reg-alloc state :integer)]
+        (let [[state dst] (reg-alloc state)]
           [(-> state
                (free-reg dst))
            (list (format "    mov %s, %s ;; load constant to register\n"
@@ -326,7 +305,7 @@
            "st(0)"])
 
     :else
-        (let [[state dst] (reg-alloc state :integer)
+        (let [[state dst] (reg-alloc state)
               addr (addrof sym-id)]
           [(-> state
                (free-reg dst))
