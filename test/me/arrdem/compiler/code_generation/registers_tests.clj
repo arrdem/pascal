f055efadca3de45ccef2420da388a7f99ca7f24a
Reid McKenzie <reid.mckenzie@calxeda.com>
2013-06-07 14:04:48 -0500

    Some cleanups to the register tests
     - move register system tests into their own file
     - add tests for the register? and free-reg fns

---
+++ test/me/arrdem/compiler/code_generation/registers_tests.clj
@@ -0,0 +1,31 @@
+(ns me.arrdem.compiler.code-generation.registers-tests
+  (:require [me.arrdem.compiler.code-generation.registers :refer :all]))
+
+(deftest free-reg-test
+  (is (= (free-reg {:used-regs #{"a"}} "a")
+         {:free-regs #{"a"}
+          :used-regs #{}}))
+
+  (is (= (free-reg {} "a")
+         {})))
+
+(deftest use-reg-test
+  (is (= (use-reg {:used-regs #{}
+                   :free-regs #{}}
+                  "a")
+         {:used-regs #{"a"}
+          :free-regs #{}})))
+
+(deftest register?-test
+  (is (= (register? {:used-regs #{}
+                     :free-regs #{"a"}}
+                    "a")
+         true))
+  (is (= (register? {:used-regs #{"a"}
+                     :free-regs #{}}
+                    "a")
+         true))
+  (is (= (register? {:used-regs #{}
+                     :free-regs #{}}
+                    "a")
+         false)))
